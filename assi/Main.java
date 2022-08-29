6d489e0b512ca5fd749242388662c3fd2dc4953b
Chandan Kumar Sharma <101566029+chandan0531@users.noreply.github.com>
2022-08-30 00:14:34 +0530

    Update Main.java

--- assi/Main.java
+++ assi/Main.java
@@ -3,7 +3,7 @@ package com.assi;
 public class Main {
 	public static void main(String[] args) {
 		
-		int[] arr = {1,2,3,4,5};
+		int[] arr = {1,2,3,4,5,6};
 		
 		for(int i=arr.length-1;i>=0; i-- )
 		{
